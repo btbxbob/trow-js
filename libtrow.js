e5a01796ef633012d61a71b05937b7b1c635b12d
BX <btbxbob@gmail.com>
2013-11-01 18:51:40 +0800

    trying to get time from server

---
+++ libtrow.js
@@ -0,0 +1,14 @@
+function libtrow($,Appsecret)
+{
+	this.stime=0;
+	this.appsecret=Appsecret;
+	function time()
+	{
+		$.getJSON('http://trow.cc/api/stats/time', function(json, textStatus) {
+				/*optional stuff to do after success */
+				alert(json);
+				this.stime=json;
+		});
+	}
+	this.time=time;
+}
