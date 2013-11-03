9baec47ef1558e1a3ea849f130b7af73844db728
btbxbob <btbxbob@gmail.com>
2013-11-04 01:01:42 +0800

    working on login too many callbacks

--- libtrow.js
+++ libtrow.js
@@ -1,64 +1,101 @@
-define(['jquery','jquery.crypt'],
+define(['jquery', 'jquery.crypt'],
 	function(jquery, nil)
 	{
 		return function(Appkey, Appsecret)
 		{
-			var $=jquery;
-			this.last=event.timeStamp; //inthel said the token will timeout in 5 minutes, about 300,000 ms.
-			this.servertime=0; //time get frome server
-			var appsecret=Appsecret;
-			this.appkey=Appkey;
+			var $ = jquery;
+			this.last = 0; //inthel said the token will timeout in 5 minutes, about 300,000 ms.
+			this.servertime = 0; //time get frome server
+			var appsecret = Appsecret;
+			this.appkey = Appkey;
 			var apptoken; //calc by calcAppToken()
-			this.uid=""; //get by getLogin
-			this.utoken=""; //get by getLogin
-			this.getTime=function(callback)
+			this.uid = ""; //get by getLogin
+			this.utoken = ""; //get by getLogin
+			this.getTime = function(callback)
 			{
-				$.ajaxSetup({cache:false});
-				$.getJSON('http://trow.cc/api/stats/time', $.proxy(function(json, textStatus) 
+				$.ajaxSetup(
 				{
-						this.servertime=json.data.time;
-						this.last=event.timeStamp; //renew last time
-						if (typeof(callback)==="function") 
-							{
-								callback();
-							}
+					cache: false
+				});
+				$.getJSON('http://trow.cc/api/stats/time', $.proxy(function(json, textStatus)
+				{
+					this.servertime = json.data.time;
+					this.last = event.timeStamp; //renew last time
+					this.calcAppToken(); //recalc token
+					if (typeof(callback) === "function")
+					{
+						callback();
+					}
 				}, this));
 			}
-		    this.calcAppToken=function()
-		    {
-		    	//example: $apptoken = md5($appkey.sha1($appsecret.$t))
-		    	var md5 =function(source){return $().crypt({method:"md5",source:source})}
-        		var sha1 =function(source){return $().crypt({method:"sha1",source:source})}
-		    	apptoken= md5(this.appkey + sha1(appsecret+ this.servertime));
-		    }
-		    this.getLogin=function(uname, ucode)
-		    {
-		    	var inner=function()
-		    	{
-	    			this.calcAppToken();
-			    	//must post
-			    	$.post('https://trow.cc/api/members/login',
-			    		{
-			    			't':this.servertime,
-			    			'apptoken':apptoken,
-			    			'appkey':this.appkey,
-			    			'uname':uname,
-			    			'ucode':ucode
-			    		} ,
-			    		$.proxy(function(json, textStatus) {
-							this.uid=json.data.uid;
-							this.utoken=json.data.utoken;
+			this.calcAppToken = function()
+			{
+				//example: $apptoken = md5($appkey.sha1($appsecret.$t))
+				var md5 = function(source)
+				{
+					return $().crypt(
+					{
+						method: "md5",
+						source: source
+					})
+				}
+				var sha1 = function(source)
+				{
+					return $().crypt(
+					{
+						method: "sha1",
+						source: source
+					})
+				}
+				apptoken = md5(this.appkey + sha1(appsecret + this.servertime));
+			}
+			this.getLogin = function(uname, ucode, callback)
+			{
+				var inner = $.proxy(function()
+				{
+					//must post
+					$.post('https://trow.cc/api/members/login',
+						{
+							't': this.servertime,
+							'apptoken': apptoken,
+							'appkey': this.appkey,
+							'uname': uname,
+							'ucode': ucode
+						},
+						$.proxy(function(json, textStatus)
+						{
+							if (json.code == 42)
+							{
+								this.uid = json.data.uid;
+								this.utoken = json.data.utoken;
+								if (typeof(callback) === "function")
+								{
+									callback();
+								}
+							}
+							else
+							{
+								throw (json.message);
+							}
 						}, this)
 					);
-		    	}
-		    	//get time first
-		    	this.getTime( //so we need a callback
-		    		$.proxy(function() 
-		    		{
-		    			inner();
-					}, this)
-				);
-		    }
+				}, this);
+
+				//get time first
+				if (+new Date - this.last > 200000)
+				{
+					this.getTime( //so we need a callback
+						$.proxy(function()
+						{
+							inner();
+						}, this)
+					);
+				}
+				else
+				{
+					inner();
+				}
+			}
 		}
 	}
 )
--- main.js
+++ main.js
@@ -2,10 +2,19 @@ require(["jquery-2.0.3.min","jquery.terminal-0.7.7.min"]
 	);
 require(["domready!","libtrow"],function(doc,libtrow) //run when dom is ready
 {
+		tr=new libtrow("965cf93c", "773c7406f8e2b167c317b95dd579a83f")
+
 		function trow_login(name,pass,callback) {
 			// body...
+			try {
+			tr.getLogin(name,pass);
+			}catch(e)
+			{
+				console.log("trow_login "+e);
+			}
 			callback(123);
 		}
+		
 		$(function($, undefined) {
 		    $('#terminal1 ').terminal(function(command, term) {
 		        if (command !== '') {
@@ -23,12 +32,15 @@ require(["domready!","libtrow"],function(doc,libtrow) //run when dom is ready
 		        }
 		    }, {
 		        greetings: 'TROW 命令行访问',
-		        name: 'js_demo',
-		        height: 200,
+		        name: 'term1',
+		        height: 250,
 		        //tabcompletion: true,
 		        login: trow_login,
-		        prompt: 'js> '});
+		        prompt: "> ",
+		        outputLimit :0
+		    });
+
 		});
 
-		tr=new libtrow("965cf93c", "773c7406f8e2b167c317b95dd579a83f")
+		
 })
